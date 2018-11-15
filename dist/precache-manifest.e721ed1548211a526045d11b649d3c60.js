8e5aa5f70c71d4efc74229c01acb2ab29403a369
yangyufeng <1539558157@qq.com>
2018-11-15 18:20:51 +0800

    项目打包git托管

---
+++ dist/precache-manifest.e721ed1548211a526045d11b649d3c60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e4986e08845c327d750",
+    "url": "/static/js/main.1e4986e0.chunk.js"
+  },
+  {
+    "revision": "3daf17738b6a3d7c0929",
+    "url": "/static/js/1.3daf1773.chunk.js"
+  },
+  {
+    "revision": "1e4986e08845c327d750",
+    "url": "/static/css/main.9d0ee282.chunk.css"
+  },
+  {
+    "revision": "170aea1afdd2cdaf2431c1410c1702b4",
+    "url": "/index.html"
+  }
+];
